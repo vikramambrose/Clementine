9cd6303c2f0b28021c539782c1bc96860aedc7e7
santigl <santix91@gmail.com>
2016-07-29 20:41:13 -0300

    Add StreamDetails Dialog

--- src/ui/streamdetailsdialog.cpp
+++ src/ui/streamdetailsdialog.cpp
@@ -1,14 +1,33 @@
 #include "streamdetailsdialog.h"
 #include "ui_streamdetailsdialog.h"
 
-StreamDetailsDialog::StreamDetailsDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::StreamDetailsDialog)
-{
-    ui->setupUi(this);
+#include <QDialogButtonBox>
+
+StreamDetailsDialog::StreamDetailsDialog(QWidget* parent)
+    : QDialog(parent), ui(new Ui::StreamDetailsDialog) {
+  ui->setupUi(this);
 }
 
-StreamDetailsDialog::~StreamDetailsDialog()
-{
-    delete ui;
+StreamDetailsDialog::~StreamDetailsDialog() { delete ui; }
+
+void StreamDetailsDialog::setUrl(const QString& url) {
+  ui->url->setText(url);
+  ui->url->setCursorPosition(0);
 }
+void StreamDetailsDialog::setFormat(const QString& format) {
+  ui->format->setText(format);
+}
+void StreamDetailsDialog::setBitrate(unsigned int bitrate) {
+  ui->bitrate->setText(QString("%1 kbps").arg(bitrate / 1000));
+}
+void StreamDetailsDialog::setChannels(unsigned int channels) {
+  ui->channels->setText(QString::number(channels));
+}
+void StreamDetailsDialog::setDepth(unsigned int depth) {
+  ui->depth->setText(QString("%1 bits").arg(depth));
+}
+void StreamDetailsDialog::setSampleRate(unsigned int sample_rate) {
+  ui->sample_rate->setText(QString("%1 Hz").arg(sample_rate));
+}
+
+void StreamDetailsDialog::on_buttonBox_rejected() { this->close(); }
--- src/ui/streamdetailsdialog.h
+++ src/ui/streamdetailsdialog.h
@@ -7,16 +7,26 @@ namespace Ui {
 class StreamDetailsDialog;
 }
 
-class StreamDetailsDialog : public QDialog
-{
-    Q_OBJECT
+class StreamDetailsDialog : public QDialog {
+  Q_OBJECT
 
-public:
-    explicit StreamDetailsDialog(QWidget *parent = 0);
-    ~StreamDetailsDialog();
+ public:
+  explicit StreamDetailsDialog(QWidget* parent = 0);
+  ~StreamDetailsDialog();
 
-private:
-    Ui::StreamDetailsDialog *ui;
+  void setUrl(const QString& url);
+  void setFormat(const QString& codec);  // This is localized, so only for human
+                                         // consumption.
+  void setBitrate(unsigned int);
+  void setDepth(unsigned int);
+  void setChannels(unsigned int);
+  void setSampleRate(unsigned int);
+
+ private slots:
+  void on_buttonBox_rejected();
+
+ private:
+  Ui::StreamDetailsDialog* ui;
 };
 
-#endif // STREAMDETAILSDIALOG_H
+#endif  // STREAMDETAILSDIALOG_H
